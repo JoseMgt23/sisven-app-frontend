926ab30c8867c28e9fc3405073cd855246752dad
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-05-22 13:22:23 -0500

    Agregar botones de editar,eliminar y agregar de Product

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -12,12 +12,23 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="(producto,index) in productos" :key="index">
+            <tr v-for="(product,index) in products" :key="index">
                 <th scope="row">{{ index+1 }}</th>
-                <td>{{ producto.name }}</td>
-                <td>{{ producto.price }}</td>
-                <td>{{ producto.stock }}</td>
-                <td>{{ producto.id_categorias }}</td>
+                <td>{{ product.name }}</td>
+                <td>{{ product.price }}</td>
+                <td>{{ product.stock }}</td>
+                <td>{{ product.id_categorias }}</td>
+                <td>
+                    <button @click="deleteProduct(product.id)"
+                        class="btn btn-danger mx-2">
+                        <font-awesome-icon icon="trash" />
+                    </button>
+                    <button @click="editProduct(product.id)"
+                        class="btn btn-warning mx-2">
+                        <font-awesome-icon icon="pencil" />
+
+                    </button>
+                </td>
             </tr>
         </tbody>
     </table>
@@ -31,6 +42,31 @@ export default {
     data(){
         return {
             products: []
+        }
+    },
+    methodos: {
+        deleteProduct(codigo){
+            Swal.fire({
+                title:'Do you want to delete the Product with id ${codigo}?',
+                showCancelButton: true,
+                confirmButtonText: 'Delete',
+            }).then((result)=> {
+                if(result.isConfirmed){
+                    axios.delete('http://127.0.0.1:8000/api/products/${codigo}')
+                    .then(response => {
+                        if(response.data.success){
+                            Swal.fire('Deleted!!','','success')
+                            this.products = response.data.products
+                        }
+                    })
+                }
+            })
+        },  
+        editProduct(id){
+            this.$router.push({name:'EditarProduct', params: {id:'${id}'}})
+        },
+        newProduct(){
+            this.$route.push({name:'NewProduct'});
         }
     },
     mounted (){
